dbed650f269c524e4f9f46dcb5ec96f7970caedc
Aleksandr Savchenko <a.savchenko@artk.ai>
2026-05-28 17:46:22 +0100

    Initial commit

---
+++ Github ExplorerTests/Github_ExplorerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExplorerTests.swift
+//  Github ExplorerTests
+//
+//  Created by Alex on 28.05.2026.
+//
+
+import Testing
+@testable import Github_Explorer
+
+struct Github_ExplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
